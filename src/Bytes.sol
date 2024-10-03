e8f70ea9ce8064b50d122fed64f1538f5e26e244
panditdhamdhere <dhamdherepandit@gmail.com>
2024-10-03 08:16:20 +0530

    struct practiceing

---
+++ src/Bytes.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.26;
+
+contract Bytes {
+    // 1 byte = 8 bit
+    // 1 hexadecimal = 4 bit
+    // 1 byte = 2 hex digit
+
+    // bytes range === 0-32
+    bytes5 public byt;
+}
---
+++ src/Struct2.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.26;
+
+contract Struct {
+    struct Employee {
+        string name;
+        uint256 age;
+        address account;
+    }
+
+    Employee public employee;
+    Employee[] public employees;
+
+    // function
+    function setValue() public {
+        Employee memory employee1 = Employee(
+            "PAndit",
+            27,
+            0x7a3dE6e00725F28d3785d089B55c7Da5C0059fe0
+        );
+        employee = employee1;
+
+        Employee memory employee2 = Employee({
+            name: "Pranav",
+            age: 22,
+            account: msg.sender
+        });
+
+        Employee memory employee3;
+        employee3.name = "Yash";
+        employee3.age = 23;
+        employee3.account = msg.sender;
+
+        employees.push(employee1);
+        employees.push(employee2);
+        employees.push(employee3);
+    }
+}

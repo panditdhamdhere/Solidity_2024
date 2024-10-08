229f7ae8e3016df8e0985a1bcfc76abd8001ed08
panditdhamdhere <dhamdherepandit@gmail.com>
2024-10-08 23:41:25 +0530

    counter added

---
+++ src/counter.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Counter {
+    uint256 public count;
+
+
+    function increment() public {
+        count = count++;
+    }
+
+    function decrement() public {
+        
+    }
+}
